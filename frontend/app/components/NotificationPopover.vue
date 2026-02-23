9b824a2544d195051550437950f57c7e8dd0cfa3
loykasi <loykasdanhcn@gmail.com>
2026-02-23 13:17:35 +0700

    add notification popover component

---
+++ frontend/app/components/NotificationPopover.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const open = ref(false);
+
+function toggle() {
+  open.value = !open.value;
+}
+</script>
+<template>
+  <UPopover
+    v-model:open="open"
+    :content="{
+      align: 'end',
+    }"
+    arrow
+  >
+    <template #anchor>
+      <UChip :show="true">
+        <UButton
+          icon="material-symbols:notifications-outline"
+          color="neutral"
+          variant="ghost"
+          @click="toggle"
+        />
+      </UChip>
+    </template>
+
+    <template #content>
+      <div class="size-48 m-4 inline-flex" />
+    </template>
+  </UPopover>
+</template>
