44cc11e7f9265c7e17b32aa68c8747b38b5d3b1a
loykasi <loykasdanhcn@gmail.com>
2026-03-04 22:22:17 +0700

    update project card skeleton

--- frontend/app/components/explore/ProjectList.vue
+++ frontend/app/components/explore/ProjectList.vue
@@ -7,21 +7,16 @@ defineProps<{
   pagination: Pagination<ProjectResponse> | undefined;
 }>();
 
-const pageSize = 6;
+const pageSize = 4;
 </script>
 <template>
   <template v-if="pending && !pagination">
-    <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-4">
-      <div
+    <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <USkeleton
         v-for="item in pageSize"
         :key="item"
-        class="rounded-lg overflow-hidden bg-default ring ring-default divide-y divide-default"
-      >
-        <USkeleton class="aspect-square w-full" />
-        <div class="p-4">
-          <USkeleton class="w-full h-[84px]" />
-        </div>
-      </div>
+        class="h-32 w-full"
+      />
     </div>
   </template>
   <template v-else-if="pagination && pagination.size > 0">
