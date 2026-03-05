dbf81bd0339d7d0169f406fc2be0234dfbad3340
loykasi <loykasdanhcn@gmail.com>
2026-03-05 23:38:23 +0700

    add user not found UI and remove unnecessary code

--- frontend/app/pages/profile/[username].vue
+++ frontend/app/pages/profile/[username].vue
@@ -7,8 +7,6 @@ const { user, isLoggedIn } = useAuthStore();
 const route = useRoute();
 const username = route.params.username as string;
 
-console.log(username);
-
 const { getProfileDetail } = useAccount();
 const { data: profile, pending: profilePending } = await useLazyAsyncData(
   `${username}`,
@@ -49,107 +47,89 @@ useHead({
 });
 </script>
 <template>
-  <UPage>
-    <template v-if="profilePending">
-      <div class="h-[40px] my-6">
-        <USkeleton class="w-40 h-full" />
-      </div>
+  <div>
+    <template v-if="!profilePending && !profile">
+      <UEmpty
+        icon="material-symbols:sentiment-stressed-outline"
+        title="No user found"
+        description="Make sure you've type the url correctly."
+      />
     </template>
-    <template v-else>
-      <h1 class="my-6 font-bold text-4xl">{{ profile?.username }}</h1>
-    </template>
+    <UPage v-else>
+      <template v-if="profilePending">
+        <div class="h-[40px] my-6">
+          <USkeleton class="w-40 h-full" />
+        </div>
+      </template>
+      <template v-else>
+        <h1 class="my-6 font-bold text-4xl">{{ profile?.username }}</h1>
+      </template>
 
-    <UCard class="mt-4">
-      <table>
-        <template v-if="profilePending">
-          <tbody>
-            <tr>
-              <td class="pe-8 py-1.5 font-medium text-default">
-                {{ $t("common.fields.username") }}
-              </td>
-              <td><USkeleton class="w-20 h-6" /></td>
-            </tr>
-            <tr>
-              <td class="pe-8 py-1.5 font-medium text-default">
-                {{ $t("profile.total_projects") }}
-              </td>
-              <td><USkeleton class="w-20 h-6" /></td>
-            </tr>
-          </tbody>
-        </template>
-        <template v-else>
-          <tbody>
-            <tr>
-              <td class="pe-8 py-1.5 font-medium text-default">
-                {{ $t("common.fields.username") }}
-              </td>
-              <td>{{ profile?.username }}</td>
-            </tr>
-            <tr>
-              <td class="pe-8 py-1.5 font-medium text-default">
-                {{ $t("profile.total_projects") }}
-              </td>
-              <td>{{ profile?.totalProject }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </table>
-    </UCard>
-
-    <div
-      class="flex items-center justify-between gap-x-4 mt-8 border-b border-b-default pb-2"
-    >
-      <h2 class="block text-3xl font-bold">{{ $t("profile.tabs.projects") }}</h2>
+      <UCard class="mt-4">
+        <table>
+          <template v-if="profilePending">
+            <tbody>
+              <tr>
+                <td class="pe-8 py-1.5 font-medium text-default">
+                  {{ $t("common.fields.username") }}
+                </td>
+                <td><USkeleton class="w-20 h-6" /></td>
+              </tr>
+              <tr>
+                <td class="pe-8 py-1.5 font-medium text-default">
+                  {{ $t("profile.total_projects") }}
+                </td>
+                <td><USkeleton class="w-20 h-6" /></td>
+              </tr>
+            </tbody>
+          </template>
+          <template v-else>
+            <tbody>
+              <tr>
+                <td class="pe-8 py-1.5 font-medium text-default">
+                  {{ $t("common.fields.username") }}
+                </td>
+                <td>{{ profile?.username }}</td>
+              </tr>
+              <tr>
+                <td class="pe-8 py-1.5 font-medium text-default">
+                  {{ $t("profile.total_projects") }}
+                </td>
+                <td>{{ profile?.totalProject }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </table>
+      </UCard>
 
       <div
-        v-if="isLoggedIn && user?.username == profile?.username"
-        class="flex items-center gap-x-2"
+        class="flex items-center justify-between gap-x-4 mt-8 border-b border-b-default pb-2"
       >
-        <UButton
-          :color="getTabColor('all')"
-          :variant="getTabVariant('all')"
-          @click="toAllTab()"
+        <h2 class="block text-3xl font-bold">{{ $t("profile.tabs.projects") }}</h2>
+
+        <div
+          v-if="isLoggedIn && user?.username == profile?.username"
+          class="flex items-center gap-x-2"
         >
-          {{ $t("profile.tabs.all") }}
-        </UButton>
-        <UButton
-          :color="getTabColor('trash')"
-          :variant="getTabVariant('trash')"
-          @click="toTrashTab()"
-        >
-          {{ $t("profile.tabs.trash") }}
-        </UButton>
+          <UButton
+            :color="getTabColor('all')"
+            :variant="getTabVariant('all')"
+            @click="toAllTab()"
+          >
+            {{ $t("profile.tabs.all") }}
+          </UButton>
+          <UButton
+            :color="getTabColor('trash')"
+            :variant="getTabVariant('trash')"
+            @click="toTrashTab()"
+          >
+            {{ $t("profile.tabs.trash") }}
+          </UButton>
+        </div>
       </div>
-    </div>
 
-    <UserProjects v-if="tab === 'all'" />
-    <UserTrash v-if="tab === 'trash'" />
-    <!-- <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
-			<template v-if="projectPagePending">
-				<div
-				v-for="item in pageSize"
-				:key="item"
-				class="rounded-lg overflow-hidden bg-default ring ring-default divide-y divide-default"
-				>
-					<USkeleton class="aspect-square w-full" />
-					<div class="p-4 h-[84px]">
-						<USkeleton class="size-full" />
-					</div>
-				</div>
-			</template>
-			<template v-else>
-				<ProjectCard
-					v-if="tab === 'all'"
-					v-for="project in projectPage?.items"
-					:project="project"
-				/>
-
-				<ProjectCard
-					v-if="tab === 'trash'"
-					v-for="project in trashPage?.items"
-					:project="project"
-				/>
-			</template>
-        </div> -->
-  </UPage>
+      <UserProjects v-if="tab === 'all'" />
+      <UserTrash v-if="tab === 'trash'" />
+    </UPage>
+  </div>
 </template>
